c90ff9979c294f01a005739431451505b7c2dd35
Zain Stiffler <19stizai@hawken.edu>
2017-07-13 00:05:56 -0500

    Working Game, I just need to handle when the player loses; Also a pause feature and start menu would be nice. MileStones!

---
+++ Station-Switcher/Assets/Scripts/addTicks.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addTicks : MonoBehaviour {
+    private float space = 0;
+    public GameObject fullTick;
+    public GameObject halfTick;
+    public int numTicks;
+	// Use this for initialization
+
+	void Awake () {
+        space = (this.GetComponent<Renderer>().bounds.size.x) / numTicks; //distance between each tick is equal and takes up full length of slider
+
+        for (int i = 0; i < numTicks; i++) { //loop adds each tick, evens being full oods being half,
+            GameObject thing;
+            if (i % 2 == 0)
+                thing = fullTick;
+            else
+                thing = halfTick;
+           Vector3 pos = new Vector3((float)(-3.795 + space * i), (float)0.15, 0); //position of each tick is the space between each starting at the left end of the slider
+            
+           GameObject tick = Instantiate(thing); //create tick
+            tick.transform.parent = this.transform; //make slider parent of slider
+            tick.transform.localPosition = pos; //move the tick to the correct position
+        }
+	}
+	
+}
